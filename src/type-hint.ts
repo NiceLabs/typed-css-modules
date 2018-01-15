022fd74995db02fd664942f1c29661c43077777a
Septs <github@septs.pw>
2018-01-15 19:37:07 +0800

    modify

--- src/type-hint.ts
+++ src/type-hint.ts
@@ -3,19 +3,38 @@ import ts = require("typescript");
 
 export function createTypeHint(tokens: string[]) {
     if (_.isEmpty(tokens)) { return ""; }
-    const members = _.chain(tokens)
+    const Locals = _.chain(tokens)
         .sortedUniq()
-        .filter((token) => /^[$_]?[^-]*$/g.test(token))
-        .map((token) => ts.createVariableDeclaration(
-            token,
-            ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+        .map((token) => ts.createPropertySignature(
+            undefined, ts.createLiteral(token), undefined,
+            ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword), undefined,
         ))
-        .map((declaration) => ts.createVariableStatement(
-            [ts.createToken(ts.SyntaxKind.ExportKeyword)],
-            ts.createVariableDeclarationList([declaration], ts.NodeFlags.Const),
+        .thru(ts.createTypeLiteralNode)
+        .thru((node) => ts.createTypeAliasDeclaration(
+            undefined, undefined, ts.createIdentifier("Locals"), undefined, node,
         ))
         .value();
+    const localsName = ts.createIdentifier("locals");
+    const locals = ts.createVariableStatement(
+        [ts.createToken(ts.SyntaxKind.DeclareKeyword)],
+        ts.createVariableDeclarationList(
+            [ts.createVariableDeclaration(
+                localsName,
+                ts.createTypeReferenceNode(
+                    Locals.name,
+                    undefined,
+                ),
+            )],
+            ts.NodeFlags.Const,
+        ),
+    );
+
     const file = ts.createSourceFile("", "", ts.ScriptTarget.Latest);
-    file.statements = ts.createNodeArray(members);
+    file.statements = ts.createNodeArray([
+        Locals,
+        locals,
+        ts.createExportAssignment(undefined, undefined, true, localsName),
+    ]);
+
     return ts.createPrinter().printFile(file);
 }
