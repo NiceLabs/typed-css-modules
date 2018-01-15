b4d7751c7f359ca2f793577f9838ed2a87efbe21
Septs <github@septs.pw>
2018-01-15 18:40:23 +0800

    modify

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { createTypeHint } from "./type-hint";
 export { getModuleTokens } from "./modules";
+export { default as TypedCSSModulesLoader } from "./extensions/webpack-loader";
+export { default as TypedCSSModulesPlugin } from "./extensions/webpack-plugin";
--- src/type-hint.ts
+++ src/type-hint.ts
@@ -3,23 +3,19 @@ import ts = require("typescript");
 
 export function createTypeHint(tokens: string[]) {
     if (_.isEmpty(tokens)) { return ""; }
-    const exportName = ts.createIdentifier("CSSModuleNames");
+    const members = _.chain(tokens)
+        .sortedUniq()
+        .filter((token) => /^[$_]?[^-]*$/g.test(token))
+        .map((token) => ts.createVariableDeclaration(
+            token,
+            ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+        ))
+        .map((declaration) => ts.createVariableStatement(
+            [ts.createToken(ts.SyntaxKind.ExportKeyword)],
+            ts.createVariableDeclarationList([declaration], ts.NodeFlags.Const),
+        ))
+        .value();
     const file = ts.createSourceFile("", "", ts.ScriptTarget.Latest);
-    file.statements = ts.createNodeArray([
-        ts.createInterfaceDeclaration(
-            undefined, undefined, exportName,
-            undefined, undefined, _.map(_.sortBy(_.uniq(tokens)), (token) => ts.createPropertySignature(
-                undefined,
-                /^[$_]?[a-zA-Z0-9]*$/g.test(token)
-                    ? ts.createIdentifier(token)
-                    : ts.createLiteral(token),
-                undefined,
-                ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-                undefined,
-            )),
-        ),
-        ts.createIdentifier("\r") as ts.Node as ts.Statement,
-        ts.createExportAssignment(undefined, undefined, true, exportName),
-    ]);
+    file.statements = ts.createNodeArray(members);
     return ts.createPrinter().printFile(file);
 }
