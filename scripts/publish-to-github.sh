94528af090077f88d6c8aea8a250d53e615213bd
Septs <github@septs.pw>
2020-07-30 16:45:46 +0800

    update scripts

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
-echo $(jq '.name = "@nicelabs/typed-css-modules"' package.json) > package.json
+jq '.name = "@nicelabs/typed-css-modules"' package.json > package-modified.json
+mv package-modified.json package.json
 
 VERSION=$(jq -r '.version' package.json)
 npm --no-git-tag-version version "$VERSION-$GITHUB_RUN_NUMBER"
